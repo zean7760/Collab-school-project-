3fba0d76a3c72bbb1fc3ac781b73bd62ab63e8c3
zean7760 <zean7760@colrado.edu>
2018-09-04 11:37:54 -0600

    inital commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Collaborate
+
+- Collaborate is an app that allows users to connect with others who want to work on projects in a collaborative setting.
+
+## Getting Ready
+
+- `yarn install`
+
+## Starting it Up
+
+- `yarn dev run`
+
+ This React App will start on port 3000.
---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import Layout from '../components/MyLayout.js';
+
+
+export default () => (
+    <Layout>
+     <header class="layout">
+      <div class="container">
+	   <div class="row text-center">
+		<div class="col-lg-10 col-lg-offset-1">
+		 <h1>Colaborate</h1>
+		</div>
+		<div class="col-lg-10 col-lg-offset-1">
+		 <h4>Find &middot; Contact  &middot; Create</h4>
+		 <a class="kafe-btn kafe-btn-default top-cart" href=""><span> Create an Account</span></a>
+		</div>
+	   </div>
+	  </div>
+     </header>
+
+     <section class="demo">
+	  <div class="container">
+	   <div class="row text-center">
+	    <div class="section-title-bold-home">
+		 <h1>Creative Industies Social Network</h1>
+		</div>
+        <div class="separator_wrapper">
+		 <div class="separator_first_circle">
+		  <div class="separator_second_circle"></div>
+		 </div>
+        </div>
+	   </div>
+	  </div>
+     </section>
+    </Layout>
+)
