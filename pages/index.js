3aeb987649fc5dfcef51a14456291d3b76ca87b6
zean7760 <zean7760@colrado.edu>
2018-09-16 19:40:03 -0600

    Added a .gitignore and connected css to index

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,27 @@
 import Layout from '../components/MyLayout.js';
-
+import React from 'react';
+import Head from 'next/head';
 
 export default () => (
-    <Layout>
-     <header class="layout">
-      <div class="container">
-	   <div class="row text-center">
-		<div class="col-lg-10 col-lg-offset-1">
-		 <h1>Colaborate</h1>
+	<div>
+	<Head>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css" />
+      <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" />
+  	<link rel="stylesheet" href="/static/style.css" />
+    </Head>
+     <header className="layout">
+      <div className="container">
+	   <div className="row text-center">
+		<div className="col-lg-10 col-lg-offset-1">
+		 <h1>Collaborate</h1>
 		</div>
-		<div class="col-lg-10 col-lg-offset-1">
+		<div className="col-lg-10 col-lg-offset-1">
 		 <h4>Find &middot; Contact  &middot; Create</h4>
-		 <a class="kafe-btn kafe-btn-default top-cart" href=""><span> Create an Account</span></a>
+		 <a className="kafe-btn kafe-btn-default top-cart" href=""><span> Create an Account</span></a>
 		</div>
 	   </div>
 	  </div>
      </header>
-
      <section class="demo">
 	  <div class="container">
 	   <div class="row text-center">
@@ -31,5 +36,5 @@ export default () => (
 	   </div>
 	  </div>
      </section>
-    </Layout>
+	</div>
 )
